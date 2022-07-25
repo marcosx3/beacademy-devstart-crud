d4a4f5ee5074f1460c335aaf1b60c703f0909786
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-07-25 20:46:32 -0300

    Refatoracao

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -6,32 +6,27 @@
     <div class="container-fluid d-flex justify-content-center mb-3 mt-4">
         <h1>Lista de Produto</h1>
     </div>
-    @if ($products == null)
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <p class="d-flex justify-content-center">Não há produtos para serem listados.</p>
-        </div>
-    @else
-        <div class="container listagem-produtos">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        {{-- <th>ID</th> --}}
-                        <th>Nome</th>
-                        <th>Gênero</th>
-                        <th>Desenvolvedor</th>
-                        <th>Distribuidor</th>
-                        <th>Lançado</th>
-                        <th>Sistema Operacional</th>
-                        <th>Processador</th>
-                        <th>Memoria Ram</th>
-                        <th>Armazenamento</th>
-                        <th>Placa Grafica</th>
-                        <th>Atualizar</th>
-                        <th>Excluir</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($products as $p): ?>
+    <div class="container listagem-produtos">
+        @include('message.flash-message')
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Gênero</th>
+                    <th>Desenvolvedor</th>
+                    <th>Distribuidor</th>
+                    <th>Lançado</th>
+                    <th>Sistema Operacional</th>
+                    <th>Processador</th>
+                    <th>Memoria Ram</th>
+                    <th>Armazenamento</th>
+                    <th>Placa Grafica</th>
+                    <th>Atualizar</th>
+                    <th>Excluir</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse  ($product as $p)
                     <tr>
                         <td> {{ $p->id }} </td>
                         <td> {{ $p->gender }} </td>
@@ -46,19 +41,19 @@
                         <td>{{ $p->storage_req }} </td>
                         <td>{{ $p->video_card }} </td>
                         <td>
-                            <button class="btn btn-info d-inline-block"><a style="text-decoration: none;"
-                                    href="{{ route('product.editar', $p->id) }}">Atualizar</a></button>
+                            <a href="{{ route('product.editar', $p->id) }}" class="btn btn-warning ">Atualizar</a>
                         </td>
                         <td>
-                            <form action="{{ route('product.deletar', $p->id) }}" method="get">
-                                <button class="btn btn-danger d-inline-block"><a
-                                        style="text-decoration: none;">Excluir</a></button>
-                            </form>
+                            <a href="{{ route('product.deletar', $p->id) }}" class="btn btn-danger ">Excluir</a>
                         </td>
                     </tr>
-                    <?php endforeach ?>
-                </tbody>
-            </table>
-        </div>
-    @endif
+            </tbody>
+        </table>
+        @empty
+            <div class="alert alert-info alert-dismissible fade show" role="alert">
+                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                 <p class="d-flex justify-content-center">Não há produtos para serem listados.</p>
+            </div>
+        @endforelse
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,22 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
-// Route::get('/dashboard', function () {
+Route::middleware(['auth'])->group(function () {
 
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
+    Route::post('/produtos/cadastro', [ProductController::class, 'register']);
+    Route::get('/produtos/cadastro', [ProductController::class, 'registerView']);
 
+    Route::get('/produtos/lista', [ProductController::class, 'show'])->name('product.show');
 
-Route::middleware(['auth'])->group(function (){
-    // CADASTRA PRODUTO
-Route::post('/produtos/cadastro', [ProductController::class, 'register']);
-Route::get('/produtos/cadastro', [ProductController::class, 'registerView']);
-// LISTAR PRODUTO
-Route::get('/produtos/lista', [ProductController::class, 'show'])->name('product.show');
+    Route::post('/produtos/update/{id}', [ProductController::class, 'update'])->name('update');
+    Route::get('/produtos/atualiza/{id}', [ProductController::class, 'edit'])->name('product.editar');
 
-// ATUALIZA PRODUTO
-Route::post('/produtos/update/{id}', [ProductController::class, 'update'])->name('update');
-Route::get('/produtos/atualiza/{id}', [ProductController::class, 'edit'])->name('product.editar');
-
-// DELETA PRODUTO
-Route::post('/produtos/remove/{id}', [ProductController::class, 'remove'])->name('remove');
-Route::get('/produtos/delete/{id}', [ProductController::class, 'destroy'])->name('product.deletar');
+    Route::post('/produtos/remove/{id}', [ProductController::class, 'remove'])->name('remove');
+    Route::get('/produtos/delete/{id}', [ProductController::class, 'destroy'])->name('product.deletar');
 });
