83da4af90b52a9a18a679334bba4965afdccca1f
marcosx3 <marcos-oliveirasilva@hotmail.com>
2022-07-25 20:49:41 -0300

    refatorando

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -12,10 +10,8 @@ class ProductController extends Controller
     {
         $this->model = $p;
     }
-
     function register(Request $request)
     {
-      
         $p = $request->except('_token');
         $product = new Product();
         $product->name = $request->name;
@@ -28,13 +24,11 @@ class ProductController extends Controller
         $product->memory_ram = $request->memory_ram;
         $product->storage_req = $request->storage_req;
         $product->video_card = $request->video_card;
-        if($product->save())
-        {
-        return redirect()->route("product.show",compact('product'))->with("success","Produto cadastrado com Sucesso!");
+        if ($product->save()) {
+            return redirect()->route("product.show", compact('product'))->with("success", "Produto cadastrado com Sucesso!");
         }
-        return redirect()->route("product.show",compact('product'))->with("error","Falha! Produto não adicionado, procure suporte técnico.");
+        return redirect()->route("product.show", compact('product'))->with("error", "Falha! Produto não adicionado, procure suporte técnico.");
     }
-
     function registerView()
     {
         return view('pages.create');
@@ -43,7 +37,7 @@ class ProductController extends Controller
     function show()
     {
         $product = Product::all();
-        return view('pages.show',compact("product"));
+        return view('pages.show', compact("product"));
     }
 
     public function edit($id)
@@ -66,24 +60,18 @@ class ProductController extends Controller
         $product->memory_ram = $request->memory_ram;
         $product->storage_req = $request->storage_req;
         $product->video_card = $request->video_card;
-        
-        if($product->update())
-        {
-            return redirect()->route("product.show",compact('product'))->with("success","Produto Atualizado com Sucesso!");
+
+        if ($product->update()) {
+            return redirect()->route("product.show", compact('product'))->with("success", "Produto Atualizado com Sucesso!");
         }
-        return redirect()->route("product.show",compact('product'))->with("error","Falha! Produto não atualizado.");
+        return redirect()->route("product.show", compact('product'))->with("error", "Falha! Produto não atualizado.");
     }
-
-
     public function destroy($id)
     {
         $product = Product::find($id);
-        
-        if($product->delete())
-        {
-            return redirect()->route("product.show",compact('product'))->with("warning","Produto deletado com Sucesso!");
+        if ($product->delete()) {
+            return redirect()->route("product.show", compact('product'))->with("warning", "Produto deletado com Sucesso!");
         }
-        return redirect()->route("product.show",compact('product'))->with("danger","Falha! Produto não Deletado.");
-      
+        return redirect()->route("product.show", compact('product'))->with("danger", "Falha! Produto não Deletado.");
     }
 }
